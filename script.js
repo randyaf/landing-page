7b84edf53efed0044632b9628bb9e68a02759498
randyaf <randyaf29@gmail.com>
2022-08-05 22:47:33 +0700

    Add simple hover animation to the cards using js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// const card = document.querySelector(".card");
+// card.addEventListener("mouseover", event => {
+//     card.style.border = "10px solid red";
+//     console.log("something happening here");
+//     console.log(event.target);
+// });
+
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach (card => {
+    card.addEventListener("mouseover", event => {
+        card.style.animation = "flow-card 1000ms"
+        console.log("something happening here");
+        console.log(event.target);
+    });
+
+    card.addEventListener("mouseout", event => {
+        card.style.animation = "unflow-card 1000ms";
+    })
+});
